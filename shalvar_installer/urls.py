ee70797ab95e9894e5b326acba7eeed69736fae8
Sexort <mrmobinof@gmail.com>
2021-07-07 09:35:59 +0430

    added installer class to urls

--- shalvar_installer/urls.py
+++ shalvar_installer/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from shalvar_installer.views import IndexOfInstallerView
 
 urlpatterns = [
-
+    path('', IndexOfInstallerView.as_view(), name='shalvar_installer_class'),
 ]
