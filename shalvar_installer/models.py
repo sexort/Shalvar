2a1535da1a37e308f36bc40682b90b0bfe340912
Sexort <mrmobinof@gmail.com>
2021-07-07 09:01:04 +0430

    Created installer model

--- shalvar_installer/models.py
+++ shalvar_installer/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class ShalvarInstallerModel(models.Model):
+    install_date = models.DateTimeField(auto_now=True)
+    website_name = models.CharField(max_length=300)
+    website_description = models.TextField()
+    admin_username = models.CharField(max_length=100)
+    admin_password = models.CharField(max_length=300)
+    admin_email = models.EmailField()
+    admin_path = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.website_name
