551ffc30e517672485cd8526a9182e26df19074b
Sexort <mrmobinof@gmail.com>
2021-07-09 01:50:52 +0430

    added installer api to urls

--- shalvar_installer/urls.py
+++ shalvar_installer/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from shalvar_installer.views import IndexOfInstallerView
+from shalvar_installer.apis import ShalvarInstallerAPI
 
 urlpatterns = [
     path('', IndexOfInstallerView.as_view(), name='shalvar_installer_class'),
+    path('', ShalvarInstallerAPI.as_view(), name='shalvar_installer_api')m
 ]
