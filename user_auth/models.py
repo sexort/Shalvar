5d92d00fba7ac01d06e717a8785fbba406357a6e
Sexort <mrmobinof@gmail.com>
2021-07-09 01:33:48 +0430

    Fixed misspell in models.py

--- user_auth/models.py
+++ user_auth/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class UserProfile(models.Model):
 
-    user = models.OneToOneField(User, on_delte=models.CASCADE)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
     is_verified = models.BooleanField(default=False)
     join_date = models.DateTimeField(auto_now=True)
