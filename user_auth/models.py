7b1d9c386eb15274d961ff65a7ac17e9cfb3e40a
Sexort <mrmobinof@gmail.com>
2021-07-08 20:39:33 +0430

    Added UserProfile model

--- user_auth/models.py
+++ user_auth/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class UserProfile(models.Model):
+
+    user = models.OneToOneField(User, on_delte=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    join_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.user.username
