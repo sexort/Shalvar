669f9bf472931b74099ba515ab9f97f62811d358
Sexort <mrmobinof@gmail.com>
2021-07-09 03:03:05 +0430

    added shalvar model to admin panel

--- shalvar_installer/apis.py
+++ shalvar_installer/apis.py
@@ -1,19 +1,35 @@
 from rest_framework import generics
 from shalvar_installer.models import ShalvarInstallerModel
-from shalvar_installer.serializers import ShalvarInstallerSerializer
+from shalvar_installer.serializers import ShalvarInstallerFirstStepSerializer
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from rest_framework import permissions
 from utilities import responses
+from os import system
 
 
-class ShalvarInstallerAPI(generics.CreateAPIView):
-    serializer_class = ShalvarInstallerSerializer
+class ShalvarInstallerFirstStepAPI(generics.CreateAPIView):
+    serializer_class = ShalvarInstallerFirstStepSerializer
     permission_classes = (permissions.AllowAny,)
+    user_model = User
     model = ShalvarInstallerModel
 
     def post(self, request, *args, **kwargs):
-        serialized_data = self.request.data
+        serialized_data = ShalvarInstallerFirstStepSerializer(data=self.request.data)
         if not serialized_data.is_valid():
-            return responses.BadRequestResponse.send_response()
+            result = {'errors': serialized_data.errors}
+            system("python manage.py makemigrations ; python manage.py migrate")
+            return responses.BadRequestResponse(result=result).send_response()
 
-        print(serialized_data)
+        self.user_model.objects.create(
+            username=serialized_data.data.get('admin_username'),
+            password=make_password(serialized_data.data),
+            email=serialized_data.data.get('admin_email'),
+            first_name='مدیر',
+            last_name=serialized_data.data.get('website_name'),
+        )
+
+        self.model.objects.create(
+            **serialized_data.data
+        )
+        return responses.SuccessResponse().send_response()
