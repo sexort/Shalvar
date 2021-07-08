1fa66644c9eb9c9ba2f6aa941aa57ba6bdb1de00
Sexort <mrmobinof@gmail.com>
2021-07-09 02:17:30 +0430

    fixed responses __init__ method name

--- utilities/responses.py
+++ utilities/responses.py
@@ -6,13 +6,11 @@ from datetime import datetime
 class BaseResponse:
     def send_response(self):
         code = self.__dict__.get('code')
-        return Response({
-            self.__dict__
-        }, status=code)
+        return Response(self.__dict__, status=code)
 
 
 class BadRequestResponse(BaseResponse):
-    def __init(self, result=None, status=False, current_datetime=datetime.now(), code=st.HTTP_400_BAD_REQUEST):
+    def __init__(self, result=None, status=False, current_datetime=datetime.now(), code=st.HTTP_400_BAD_REQUEST, **kwargs):
         self.result = result
         self.status = status
         self.current_datetime = current_datetime
@@ -20,7 +18,7 @@ class BadRequestResponse(BaseResponse):
 
 
 class NotFoundResponse(BaseResponse):
-    def __init(self, result=None, status=False, current_datetime=datetime.now(), code=st.HTTP_404_NOT_FOUND):
+    def __init__(self, result=None, status=False, current_datetime=datetime.now(), code=st.HTTP_404_NOT_FOUND):
         self.result = result
         self.status = status
         self.current_time = current_datetime
