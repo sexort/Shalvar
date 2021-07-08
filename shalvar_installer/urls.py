b9f8dbb64533ac7d829627fb934321ac4272adcc
Sexort <mrmobinof@gmail.com>
2021-07-09 03:04:16 +0430

    appended api class changename on urls

--- shalvar_installer/urls.py
+++ shalvar_installer/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from shalvar_installer.views import IndexOfInstallerView
-from shalvar_installer.apis import ShalvarInstallerAPI
+from shalvar_installer.apis import ShalvarInstallerFirstStepAPI
 
 urlpatterns = [
     path('', IndexOfInstallerView.as_view(), name='shalvar_installer_class'),
-    path('api/', ShalvarInstallerAPI.as_view(), name='shalvar_installer_api'),
+    path('api/', ShalvarInstallerFirstStepAPI.as_view(), name='shalvar_installer_api'),
 ]
