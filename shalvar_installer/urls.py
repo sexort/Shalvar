257b423ffb3ec398f609749032d713ddf21d41b5
Sexort <mrmobinof@gmail.com>
2021-07-09 01:53:34 +0430

    fixed misspell in urls

--- shalvar_installer/urls.py
+++ shalvar_installer/urls.py
@@ -4,5 +4,5 @@ from shalvar_installer.apis import ShalvarInstallerAPI
 
 urlpatterns = [
     path('', IndexOfInstallerView.as_view(), name='shalvar_installer_class'),
-    path('', ShalvarInstallerAPI.as_view(), name='shalvar_installer_api')m
+    path('', ShalvarInstallerAPI.as_view(), name='shalvar_installer_api'),
 ]
