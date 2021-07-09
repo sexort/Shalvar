8d383ec585e76a07f4a34c88dfb8bb149756680f
Sexort <mrmobinof@gmail.com>
2021-07-09 06:07:09 +0430

    added step2 installer serializer

--- shalvar_installer/serializers.py
+++ shalvar_installer/serializers.py
@@ -5,6 +5,24 @@ from rest_framework import exceptions
 from validate_email import validate_email
 
 
+class ShalvarInstallerFirstStepSerializer(Serializer):
+    database_type = serializers.CharField(max_length=15)
+    database_username = serializers.CharField(allow_blank=True, allow_null=True)
+    database_password = serializers.CharField(allow_blank=True, allow_null=True)
+    database_name = serializers.CharField(allow_blank=True, allow_null=True)
+    database_port = serializers.IntegerField(allow_null=True)
+
+    def validate_database_type(self, database_type):
+        database_types: list = [
+            "mysql", "postgresql", "mongodb", "sqlite"
+        ]
+        if database_type not in database_types:
+            raise exceptions.ValidationError(_(
+                'invalid database type'
+            ))
+        return database_typeb
+
+
 class ShalvarInstallerSecondStepSerializer(Serializer):
     website_name = serializers.CharField(min_length=1, max_length=100)
     admin_path = serializers.CharField(min_length=1, max_length=50)
