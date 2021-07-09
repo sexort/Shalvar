5f40a8787636f0548e3f22407f756bec514bae40
Sexort <mrmobinof@gmail.com>
2021-07-09 06:06:55 +0430

    added first step installer api to urls

--- shalvar_installer/urls.py
+++ shalvar_installer/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from shalvar_installer.views import IndexOfInstallerView
-from shalvar_installer.apis import ShalvarInstallerSecondStepAPI
+from shalvar_installer.apis import *
 
 urlpatterns = [
     path('', IndexOfInstallerView.as_view(), name='shalvar_installer_class'),
-    path('api/', ShalvarInstallerSecondStepAPI.as_view(), name='shalvar_installer_api'),
+    path('api/second-step/', ShalvarInstallerSecondStepAPI.as_view(), name='shalvar_installer_api'),
+    path('api/databases/', CheckInstalledDatabase.as_view(), name='check_databases'),
 ]
